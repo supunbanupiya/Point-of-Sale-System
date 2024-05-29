9fe0121039f7605597366aa5236c6c4708c27c7e
Supunbanupiya <supunbanupiya@gmail.com>
2024-05-29 21:35:41 +0530

    first commit

---
+++ Point of Sale System/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Point_of_Sale_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Point_of_Sale_System.Controllers
+{
+    public class HomeController : Controller
+    {
+        possystemEntities dc = new possystemEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Point of Sale System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Point_of_Sale_System.MvcApplication" Language="C#" %>
